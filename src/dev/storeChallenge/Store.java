f3aa7b07da53f683a8de797572fe274a351dee34
Rikard <rikardengstrom89@gmail.com>
2025-07-08 20:07:10 +0200

    done

--- src/dev/storeChallenge/Store.java
+++ src/dev/storeChallenge/Store.java
@@ -1,8 +1,26 @@
 package dev.storeChallenge;
 
+import java.util.ArrayList;
+
 public class Store {
+
+    private static ArrayList<ProductForSale> storeProducts = new ArrayList<>();
 
     public static void main(String[] args) {
 
+        storeProducts.add(new ArtObject("Oil Painting", 1350, "Impressionistic work by ABF painted in 2010"));
+
+        storeProducts.add(new ArtObject("Sculpture", 2000, "Brone work by JKF, produced in 1950"));
+
+        listProducts();
+
+    }
+
+    public static void listProducts() {
+
+        for (var item : storeProducts) {
+            System.out.println("-".repeat(30));
+            item.showDetails();
+        }
     }
 }
